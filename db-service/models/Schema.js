47cf911b6a02ec956268d438bd594a3ea203733d
Galarza Villalva César Luis <132913578+DiverCesar@users.noreply.github.com>
2026-06-15 10:37:20 -0500

    Create Schema.js for data model using Mongoose

---
+++ db-service/models/Schema.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const dataSchema = new mongoose.Schema(
+    {
+        id: { type: Number },
+        name: { type: String },
+        brand: { type: String },
+        model: { type: String },
+        price: { type: Number },
+        date: { type: String },
+        description: { type: String },
+        isActive: { type: Boolean }
+    },
+    { collection: process.env.COLLECTION_NAME }
+);
+
+module.exports = mongoose.model("Data", dataSchema);
