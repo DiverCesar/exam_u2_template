b9ec427dcf747d87ebb7c6f3d748040e7b125096
Galarza Villalva César Luis <132913578+DiverCesar@users.noreply.github.com>
2026-06-17 10:52:50 -0500

    Fix console log syntax in server.js

--- api-get-service/server.js
+++ api-get-service/server.js
@@ -11,5 +11,5 @@ app.use(express.json());
 app.use("/", getRoutes);
 
 app.listen(process.env.PORT_GET, () => {
-    console.log([SUCCESS] ${process.env.APP_NAME} API GET Service is up and running for ${process.env.ITEM_PLURAL} on port ${process.env.PORT_GET});
+    console.log(`[SUCCESS] ${process.env.APP_NAME} API GET Service is up and running for ${process.env.ITEM_PLURAL} on port ${process.env.PORT_GET}`);
 });
