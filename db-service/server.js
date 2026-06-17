118d1c98ecdea3720a69d049e46a380cb057e2b5
Galarza Villalva César Luis <132913578+DiverCesar@users.noreply.github.com>
2026-06-17 10:39:01 -0500

    Enhance MongoDB connection and server startup logging
    Updated MongoDB connection to specify database name and added success log message on server start.

--- db-service/server.js
+++ db-service/server.js
@@ -9,8 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+    dbName: process.env.APP_NAME
+});
 
 app.use("/db", dbRoutes);
 
-app.listen(process.env.PORT_DB);
+app.listen(process.env.PORT_DB, () => {
+    console.log(`[SUCCESS] MongoDB Connection established. Database Service running on port ${process.env.PORT_DB}`);
+});
