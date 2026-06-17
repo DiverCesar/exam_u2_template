a7e07822cdc85817e8e1435b77078937dd249ca9
Galarza Villalva César Luis <132913578+DiverCesar@users.noreply.github.com>
2026-06-17 10:53:21 -0500

    Fix console log syntax in server.js

--- api-delete-service/server.js
+++ api-delete-service/server.js
@@ -11,5 +11,5 @@ app.use(express.json());
 app.use("/", deleteRoutes);
 
 app.listen(process.env.PORT_DELETE, () => {
-    console.log([SUCCESS] ${process.env.APP_NAME} API DELETE Service is up and running for ${process.env.ITEM_PLURAL} on port ${process.env.PORT_DELETE});
+    console.log(`[SUCCESS] ${process.env.APP_NAME} API DELETE Service is up and running for ${process.env.ITEM_PLURAL} on port ${process.env.PORT_DELETE}`);
 });
