511db33399d7d8d65d9d9c5c0777071a43cb31b9
Galarza Villalva César Luis <132913578+DiverCesar@users.noreply.github.com>
2026-06-17 10:50:00 -0500

    Update database host in delete route

--- api-delete-service/routes/deleteRoutes.js
+++ api-delete-service/routes/deleteRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.delete(`/${process.env.APP_NAME}/${process.env.ITEM_SINGULAR}/:id`, async (req, res) => {
     try {
-        const response = await fetch(`http://localhost:${process.env.PORT_DB}/db/${process.env.ITEM_SINGULAR}/${req.params.id}`, {
+        const response = await fetch(`http://${process.env.DB_HOST || 'localhost'}:${process.env.PORT_DB}/db/${process.env.ITEM_SINGULAR}/${req.params.id}`, {
             method: "DELETE"
         });
         const data = await response.json();
